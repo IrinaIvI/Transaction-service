72e89fb572f42f8f28d15a83908e16ecdbd88f87
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-07-07 17:50:08 +0000

    Изменение структуры класса Transactions

--- src/app/transaction.py
+++ src/app/transaction.py
@@ -1,5 +1,6 @@
 import time
 from dataclasses import dataclass
+from typing import Union, NoReturn
 
 tuple_time = (2019, 12, 7, 14, 30, 30, 5, 0, 0)
 init_time = time.asctime(tuple_time)
@@ -13,49 +14,58 @@ transactions = {}
 reports = {}
 
 
-@dataclass
-class Transaction:
-    """Класс транзакции."""
-
-    current_amount: float
-    transaction_time: time
-
-    def get_amount(self) -> float:
-        """Получение текущей суммы транзакции."""
-        return self.current_amount
-
-    def get_time(self) -> time:
-        """Получение даты совершения транзакции."""
-        return self.transaction_time
-
-
 class Transactions:
     """Класс для работы с транзакциями."""
 
-    def create_transaction(self, user_id: int, amount: float, trans_type: str) -> str:
+    @dataclass
+    class Transaction:
+        """Класс транзакции."""
+
+        _current_amount: Union[float, int] = 0
+        _transaction_time: time = init_time
+
+        @property
+        def amount(self) -> Union[float, int]:
+            """Получение текущей суммы транзакции."""
+            return self._current_amount
+
+        @property
+        def time(self) -> time:
+            """Получение даты совершения транзакции."""
+            return self._transaction_time
+
+    def create_transaction(self, user_id: int, amount: Union[float, int], trans_type: str) -> NoReturn:
         """Создание транзакции."""
         if user_id in users:
             current_balance = users.get(user_id)[0]
-            if trans_type == '+':
-                current_balance += amount
-            elif trans_type == '-' and current_balance >= amount:
-                current_balance -= amount
+            if amount < 0:
+                raise ValueError('Error, incorrect amount')
             else:
-                return 'Error, incorrect operation'
+                if trans_type == '+':
+                    current_balance += amount
+                elif trans_type == '-' and current_balance >= amount:
+                    current_balance -= amount
+                else:
+                    raise ValueError('Error, incorrect operation')
 
             users.get(user_id)[0] = current_balance
 
-            tr = Transaction(current_balance, time.ctime(time.time()))
+            tr = Transactions.Transaction(current_balance, time.ctime(time.time()))
             transactions.setdefault(user_id, []).append(tr)
         else:
-            return 'Error, this user is not exist'
+            raise ValueError('Error, this user does not exist')
 
     def get_transaction(self, user_id: int, start: time, end: time) -> dict:
         """Получение транзакции."""
         report = []
-        lst = transactions.get(user_id)
-        for el in lst:
-            if el.get_time() >= start or el.get_time() <= end:
-                report.append([el.get_amount(), el.get_time()])
+        if start > end:
+            raise ValueError('Error, incorrect time interval')
+        if user_id not in transactions:
+            raise ValueError('Error, this user does not exist')
+        
+        list_of_trans = transactions.get(user_id)
+        for transaction in list_of_trans:
+            if transaction.time >= start or transaction.time <= end:
+                report.append([transaction.amount, transaction.time])
                 reports.setdefault(id, []).append(report)
         return report
