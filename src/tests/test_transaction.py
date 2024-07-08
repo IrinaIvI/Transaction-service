90676d35426f10112ac490a41f6c17563107bd06
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-07-08 18:41:39 +0000

    Добавление новых тестов

--- src/tests/test_transaction.py
+++ src/tests/test_transaction.py
@@ -1,15 +1,15 @@
 import pytest
 import time
-from app.transaction import Transactions
+from app.transaction import Transactions, create_base, transactions, users, reports
 
-# @pytest.fixture
-# def create_base():
-#     tuple_time = (2019, 12, 7, 14, 30, 30, 5, 0, 0)
-#     init_time = time.asctime(tuple_time)
-#     users = {1: [2000, init_time],
-#              2: [3000, init_time],
-#              3: [4000, init_time],
-#             }
+
+@pytest.fixture
+def create_test_base():
+    transactions.clear()
+    users.clear()
+    reports.clear()
+    create_base()
+    yield
 
 @pytest.mark.parametrize('user_id, amount, transaction_type',
  [pytest.param(1, 1000, '+',  id='is correct'),
@@ -18,12 +18,25 @@ from app.transaction import Transactions
   pytest.param(1, 1000, '?', id='is not correct', marks=pytest.mark.xfail()),
   ]
 )
-def test_create_transaction(user_id, amount, transaction_type):
-    assert Transactions().create_transaction(user_id, amount, transaction_type) == None
+def test_create_transaction(user_id, amount, transaction_type, create_test_base):
+    #Arrange
+    create_test_base
+    #Act
+    transaction = Transactions().create_transaction(user_id, amount, transaction_type)
+    #Assert
+    assert transaction == None
 
 @pytest.mark.parametrize('user_id, start, end', [
-    pytest.param(1, time.time(), time.time())
+    pytest.param(1, time.ctime(time.time()), time.ctime(time.time()), id='is correct'),
+    pytest.param(4, time.ctime(time.time()), time.ctime(time.time()), id='is not correct', marks=pytest.mark.xfail())
 ])
-def test_get_transaction(user_id, start, end):
-    #Transactions().create_transaction(1, 1500, '-')
-    assert Transactions().get_transaction(user_id, start, end) == [[500, time.time()], [1000, time.time()]]
+def test_get_transaction(user_id, start, end, create_test_base):
+    #Arange
+    create_test_base
+    Transactions().create_transaction(1, 1500, '-')
+    transaction = transactions.get(user_id)[0]
+    #Act
+    resulting_report = Transactions().get_transaction(user_id, start, end)
+    report = [[500, transaction.time]]
+    #Assert
+    assert resulting_report == report
