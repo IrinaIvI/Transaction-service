96db16f0da91553a241cf17889b68a9483ea9e3a
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-08-29 15:36:43 +0000

    рефакторинг url подключения

--- src/app/database.py
+++ src/app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://username:password@postgres-service:5432/iivashko"
-#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@host.docker.internal:5432/card_app"
 
 engine = create_engine(url=SQLALCHEMY_DATABASE_URL, echo=True)
 new_session = sessionmaker(engine, expire_on_commit=False)
